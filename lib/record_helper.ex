43ef04caac98a1279a79819d4bf698a8fe9d5b59
sasa1977 <sasa.juric@gmail.com>
2013-02-28 13:51:28 +0100

    removed duplication

--- lib/record_helper.ex
+++ lib/record_helper.ex
@@ -1,22 +1,44 @@
+defmodule Workex.RecordHelper.DynGenerator do
+  defmacro __using__(_) do
+    generate_macros(function(:def_fields, 1)) ++
+    generate_macros(function(:def_this, 1)) ++
+    [def_this([])]
+  end
+
+  defp generate_macros(generator) do
+    Enum.map(1..20, fn(i) ->
+      generator.(args(i))
+    end)
+  end
+
+  defp def_fields(args) do
+    quote do 
+      defmacro fields(unquote_splicing(args)) do
+        define_fields([unquote_splicing(args)])
+      end
+    end
+  end
+
+  defp def_this(args) do
+    quote do 
+      defmacro this(unquote_splicing(args)) do
+        define_this([unquote_splicing(args)])
+      end
+    end
+  end
+
+  defp args(n) do
+    Enum.map(1..n, fn(i) -> {binary_to_atom("arg#{i}"), [], nil} end)
+  end
+end
+
 defmodule Workex.RecordHelper do
   defmacro fields(args) when is_list(args) do
     define_fields(args)
   end
 
-  defmacro fields(arg), do: define_fields([arg])
-
-  defmacro fields(arg1, arg2), do: define_fields([arg1, arg2])
-  defmacro fields(arg1, arg2, arg3), do: define_fields([arg1, arg2, arg3])
-  defmacro fields(arg1, arg2, arg3, arg4), do: define_fields([arg1, arg2, arg3, arg4])
-  defmacro fields(arg1, arg2, arg3, arg4, arg5), do: define_fields([arg1, arg2, arg3, arg4, arg5])
-
-  defmacro this(), do: define_this([])
-  defmacro this(arg1), do: define_this([arg1])
-  defmacro this(arg1, arg2), do: define_this([arg1, arg2])
-  defmacro this(arg1, arg2, arg3), do: define_this([arg1, arg2, arg3])
-  defmacro this(arg1, arg2, arg3, arg4), do: define_this([arg1, arg2, arg3, arg4])
-  defmacro this(arg1, arg2, arg3, arg4, arg5), do: define_this([arg1, arg2, arg3, arg4, arg5])
-
+  use Workex.RecordHelper.DynGenerator
+  
   defp define_this(args) do
     quote do
       unquote(define_fields(args)) = unquote({:this, [], nil})
@@ -34,5 +56,6 @@ defmodule Workex.RecordHelper do
       {unquote(field), unquote(pattern)}
     end
   end
+
   defp field(name), do: field({elem(name, 0), name})
 end

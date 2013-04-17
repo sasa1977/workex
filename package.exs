a5df12336378a62f162f3a3c40e02aa3195de7b1
sasa1977 <sasa.juric@gmail.com>
2013-04-17 10:09:22 +0200

    expm

---
+++ package.exs
@@ -0,0 +1,6 @@
+Expm.Package.new(
+  name: "workex", description: "Control over message receiving and processing in elixir workers",
+  version: "0.1", keywords: ["elixir", "messaging"], 
+  maintainers: [[name: "Sasa Juric", email: "sasa.juric@gmail.com"]],
+  repositories: [[github: "sasa1977/workex"]]
+)

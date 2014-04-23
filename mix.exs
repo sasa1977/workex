0431d0b0aa58543c788bec41e5af4c7137fe2f76
Sasa Juric <sasa.juric@gmail.com>
2014-04-23 14:11:15 +0200

    hex manifest

--- mix.exs
+++ mix.exs
@@ -2,10 +2,20 @@ defmodule Workex.Mixfile do
   use Mix.Project
 
   def project do
-    [ app: :workex,
+    [
+      app: :workex,
       version: "0.0.1",
       elixir: ">= 0.13.0",
-      deps: deps ]
+      deps: deps,
+      package: [
+        contributors: ["Saša Jurić"],
+        licenses: ["MIT"],
+        links: [{"Github", "https://github.com/sasa1977/workex"}]
+      ],
+      description: """
+        Backpressure and flow control in EVM processes.
+      """
+    ]
   end
 
   def application do

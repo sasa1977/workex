db31d5ad38810495b2fc871921ed6203d9cbfc46
Sasa Juric <sasa.juric@gmail.com>
2014-09-02 22:23:01 +0200

    upgrade to Elixir v1.0.0-rc1

--- mix.exs
+++ mix.exs
@@ -6,23 +6,23 @@ defmodule Workex.Mixfile do
   def project do
     [
       app: :workex,
-      version: "0.2.0",
-      elixir: "~> 0.15.0",
+      version: "0.3.0",
+      elixir: "~> 1.0.0-rc1",
       deps: deps,
       package: [
         contributors: ["Saša Jurić"],
         licenses: ["MIT"],
-        links: [{"Github", "https://github.com/sasa1977/workex"}]
+        links: %{"Github": "https://github.com/sasa1977/workex"}
       ],
       description: "Backpressure and flow control in EVM processes."
     ]
   end
 
   def application do
-    []
+    [applications: [:exactor, :logger]]
   end
 
   defp deps do
-    [{:exactor, "0.6.0"}]
+    [{:exactor, "0.7.0"}]
   end
 end

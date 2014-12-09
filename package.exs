5dcc1e72c807a4dfabc966aa44d2d70fce41567f
Sasa Juric <sasa.juric@gmail.com>
2014-12-09 21:26:53 +0100

    remove package.exs

--- package.exs
+++
@@ -1,6 +0,0 @@
-Expm.Package.new(
-  name: "workex", description: "Control over message receiving and processing in elixir workers",
-  version: "0.1", keywords: ["elixir", "messaging"], 
-  maintainers: [[name: "Sasa Juric", email: "sasa.juric@gmail.com"]],
-  repositories: [[github: "sasa1977/workex"]]
-)
